442f7a4f7e3c9b2ddb062e8f1c7a65df5a6435ae
rafifmulia <rafif.mulia.r@gmail.com>
2022-12-18 17:14:42 +0700

    finish 221217

--- sri_sulastiyani_proses.php
+++ sri_sulastiyani_proses.php
@@ -1,42 +1,52 @@
 <?php
-  // cek apakah inputannya ada
-  if(isset($_POST['jumlah_baris'])) {
-    // memastikan bahwa yang diinput bilangan bulat
-    if (($_POST['jumlah_baris']) > 0) {
-      $jumlah_baris = round($_POST['jumlah_baris']);
-      echo 'Jumlah baris : ' . $jumlah_baris . '<br>';
-      // fungsi rekursif
-      function rekursif($jumlah_baris_tersisa = 1, $initial = 1) {
-        $penambah = 1;
-        $temp = 0;
-        $hasil = 0;
+/**
+ * membuat deret fibonacci
+ * outputnya
+ * 1
+ * 1 1
+ * 1 1 2
+ * 1 1 2 3
+ * 1 1 2 3 5
+ * 1 1 2 3 5 8
+ */
 
-        for ($i=0; $i<$initial;) {
-          // echo ($i + $penambah).' + ';
-          // echo $penambah.' + ';
-          // echo $i.' + ';
-          echo $temp + $penambah;
+// cek apakah inputannya ada
+if(isset($_POST['jumlah_baris'])) {
+  // memastikan bahwa yang diinput bilangan bulat
+  if (($_POST['jumlah_baris']) > 0) {
+    $jumlah_baris = round($_POST['jumlah_baris']);
+    // fungsi rekursif
+    function rekursif($jumlah_baris_tersisa = 1, $deret = 1) {
+      $num1 = 1;
+      $num2 = 1;
+      $num3 = 0;
 
-          if ($i >= $initial-1) {
-            echo ' = ';
-          } else {
-            echo ' + ';
-          }
-
-          $temp = $temp+$penambah;
-          $hasil += $temp;
-
-          $i++; 
-          $penambah += 2;
-        }
-        echo $hasil.'<br>';
-
-        if ($jumlah_baris_tersisa <= 1) return '';
-        rekursif($jumlah_baris_tersisa-1, $initial+1);
+      for ($i=1; $i<=$deret; $i++) {
+        echo ' ' . $num1;
+        $num3 = $num1 + $num2;
+        $num1 = $num2;
+        $num2 = $num3;
       }
-      // menjalankan fungsi rekursif pertama kali
-      rekursif($jumlah_baris, 1);
+      echo '<br>';
+
+      if ($jumlah_baris_tersisa <= 1) return '';
+      rekursif($jumlah_baris_tersisa-1, $deret+1);
     }
+    // menjalankan fungsi rekursif pertama kali
+    rekursif($jumlah_baris);
   }
-  echo '<br><a href="sri_sulastiyani_input.php">Kembali</a>';
-?>
+}
+echo '<br> <a href="sri_sulastiyani_input.php">Kembali</a>';
+
+
+// $n = 10;
+// $num1 = 0;
+// $num2 = 1;
+// $counter = 0;
+// while ($counter < $n){
+//   echo ' '.$num1;
+//   $num3 = $num2 + $num1;
+//   $num1 = $num2;
+//   $num2 = $num3;
+//   $counter = $counter + 1;
+// }
